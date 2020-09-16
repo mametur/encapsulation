92b972fabc52d805ef4c8dae8120aa9a93de632b
Mamé <mameazad.turgut@gmail.com>
2020-09-16 11:47:04 +0200

    finish object-create

--- isolate/08-object-create/exercises/4-write-logic.js
+++ isolate/08-object-create/exercises/4-write-logic.js
@@ -2,35 +2,41 @@
 
 // delegated methods
 const userPrototype = {
-  get status() {
-    // render userName and loggedIn status into a string
-  },
-  logIn: function (password) {
-    // log the user in if they pass the correct password
-  },
-  logOut: function () {
-    // log the user out no matter what
-  },
-  changePassword: function (oldPassword, newPassword) {
-    // if the user is logged in, and the oldPassword is correct, reset their password
-  }
+	get status() {
+		return this.state.loggedIn ? `${this.state.userName} is logged in` : `${this.state.userName} is logged out`;
+		// render userName and loggedIn status into a string
+	},
+	logIn: function (password) {
+		password === this.state.password ? (this.state.loggedIn = true) : (this.state.loggedIn = false);
+		// log the user in if they pass the correct password
+	},
+	logOut: function () {
+		this.state.loggedIn = false;
+		// log the user out no matter what
+	},
+	changePassword: function (oldPassword, newPassword) {
+		// if the user is logged in, and the oldPassword is correct, reset their password
+
+		if (this.state.loggedIn) {
+			oldPassword === this.state.password ? (this.state.password = newPassword) : this.state.password;
+		}
+	},
 };
 console.log('userPrototype:', userPrototype);
-
 
 // create instances
 const userAverie = Object.create(userPrototype);
 userAverie.state = {
-  loggedIn: false,
-  userName: 'Averie',
-  password: 'brussels1000'
+	loggedIn: false,
+	userName: 'Averie',
+	password: 'brussels1000',
 };
 
 const userRory = Object.create(userPrototype);
 userRory.state = {
-  loggedIn: true,
-  userName: 'Rory',
-  password: 'HYF-BE'
+	loggedIn: true,
+	userName: 'Rory',
+	password: 'HYF-BE',
 };
 
 // test initial instances
@@ -40,13 +46,11 @@ console.assert(testAverie0, 'Averie 0: Averie is logged out');
 const testRory0 = userRory.status === 'Rory is logged in';
 console.assert(testRory0, 'Rory 0: Rory is logged in');
 
-
 // users do things
 userRory.logOut();
 userAverie.changePassword('brussels1000', 'copenhagen2000');
 userAverie.logIn('brussels1000');
 userRory.logIn('HYF-BE');
-
 
 // test intermediate states
 const testAverie1 = userAverie.status === 'Averie is logged in';
@@ -55,15 +59,13 @@ console.assert(testAverie1, 'Averie 1: Averie is logged in');
 const testRory1 = userRory.status === 'Rory is logged in';
 console.assert(testRory1, 'Rory 1: Rory is logged in');
 
-
 // users do things
 userAverie.changePassword('bruxelles1000', 'copenhagen2000');
 userAverie.logOut();
 userRory.changePassword('HYF-BE', 'HYF-CPH');
 userRory.logOut();
 userAverie.logIn('copenhagen2000');
-userRory.logIn('HYF-CPH')
-
+userRory.logIn('HYF-CPH');
 
 // test intermediate states
 const testAverie2 = userAverie.status === 'Averie is logged out';
@@ -71,7 +73,6 @@ console.assert(testAverie2, 'Averie 2: Averie is logged out');
 
 const testRory2 = userRory.status === 'Rory is logged in';
 console.assert(testRory2, 'Rory 2: Rory is logged in');
-
 
 // users do things
 userRory.logOut();
@@ -81,7 +82,6 @@ userAverie.changePassword('brussels1000', 'copenhagen2000');
 userAverie.logOut();
 userRory.logOut();
 userAverie.logIn('copenhagen2000');
-
 
 // test final state
 const testAverie3 = userAverie.status === 'Averie is logged in';
